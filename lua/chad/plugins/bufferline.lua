f572cc0799206cbc466297e757b68a5341c8bb0b
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-05-10 01:42:22 +0530

    added bufferline offset, need to work on buffer highlighting

--- lua/chad/plugins/bufferline.lua
+++ lua/chad/plugins/bufferline.lua
@@ -1,11 +1,28 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("bufferline").setup()
-    vim.keymap.set("n", "<Tab>", "<cmd> bnext <CR>")
-    vim.keymap.set("n", "<S-Tab>", "<cmd> bprev <CR>")
-    vim.keymap.set("n", "<leader>x", "<cmd> bdelete <CR>")
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+		local opts = {
+      mode = "buffers",
+			themeable = true,
+			separator_style = "slant",
+			diagnostics = false,
+			offsets = {
+				{
+          filetype = "neo-tree",
+          text = "Files",
+          highlight = "Directory",
+          separator = true,
+        },
+			},
+		}
+
+		bufferline.setup({ options = opts })
+
+		vim.keymap.set("n", "<Tab>", "<cmd> bnext <CR>")
+		vim.keymap.set("n", "<S-Tab>", "<cmd> bprev <CR>")
+		vim.keymap.set("n", "<leader>x", "<cmd> bdelete <CR>")
+	end,
 }
