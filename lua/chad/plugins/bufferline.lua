abf2121ba0d451477c329d806d08024a1930e5e3
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-05-09 23:33:46 +0530

    updated bufferline keymaps

--- lua/chad/plugins/bufferline.lua
+++ lua/chad/plugins/bufferline.lua
@@ -4,8 +4,8 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup()
-    vim.keymap.set("n", "1", "<cmd> bprev <CR>")
-    vim.keymap.set("n", "2", "<cmd> bnext <CR>")
+    vim.keymap.set("n", "<Tab>", "<cmd> bnext <CR>")
+    vim.keymap.set("n", "<S-Tab>", "<cmd> bprev <CR>")
     vim.keymap.set("n", "<leader>x", "<cmd> bdelete <CR>")
   end,
 }
