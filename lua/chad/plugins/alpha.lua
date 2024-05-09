8ed9bffea83e282ff56b19569d2d089e17f3f360
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-05-09 23:54:54 +0530

    changed alpha theme

--- lua/chad/plugins/alpha.lua
+++ lua/chad/plugins/alpha.lua
@@ -1,32 +1,57 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+	config = function()
+    vim.keymap.set('n', '<leader>a', "<cmd> Alpha <CR>")
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                            happy coding!                              ]],
-    }
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
 
-    alpha.setup(dashboard.opts)
-    vim.keymap.set('n', '<leader>a', '<cmd> Alpha <CR>')
-  end,
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+			-- wrap = "overflow";
+		}
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
+			dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+			dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
+			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
+			dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+			dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+		}
+
+		local function footer()
+			return "Happy Coding!"
+		end
+		dashboard.section.footer.val = footer()
+
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+
+		require("alpha").setup(dashboard.opts)
+	end,
 }
