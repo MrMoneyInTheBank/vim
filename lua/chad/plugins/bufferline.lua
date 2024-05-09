be81742deb9d536c92726dae5abd43e9403b6faa
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-05-09 18:25:24 +0530

    added bufferline keymaps to close buffers

--- lua/chad/plugins/bufferline.lua
+++ lua/chad/plugins/bufferline.lua
@@ -6,5 +6,6 @@ return {
     require("bufferline").setup()
     vim.keymap.set("n", "1", "<cmd> bprev <CR>")
     vim.keymap.set("n", "2", "<cmd> bnext <CR>")
+    vim.keymap.set("n", "<leader>x", "<cmd> bdelete <CR>")
   end,
 }
