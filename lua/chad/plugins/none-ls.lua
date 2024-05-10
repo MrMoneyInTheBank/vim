1af70469b359f31bf547bd21c727a2bb7bb42924
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-05-10 14:02:10 +0530

    configured nonels sources to work on particular files

--- lua/chad/plugins/none-ls.lua
+++ lua/chad/plugins/none-ls.lua
@@ -5,8 +5,9 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.mypy.with({ filetypes = { "python" } }),
+				null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
+				null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
 			},
 		})
 
