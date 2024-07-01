94012e53054db383a8d73ad7e83d2111452e5894
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-01 21:48:34 +0530

    added yank file keymap

--- lua/chad/mappings.lua
+++ lua/chad/mappings.lua
@@ -2,5 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "<leader>ii", "gg0vG$=")
+vim.keymap.set("n", "<leader>cop", "<cmd>%yank<CR>")
 
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
