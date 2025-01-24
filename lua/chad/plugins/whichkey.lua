9c2e57f788ffe3b4b0e91e16f9c884360cc75cb7
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-01-24 10:24:23 +0530

    whichkey config

--- lua/chad/plugins/whichkey.lua
+++ lua/chad/plugins/whichkey.lua
@@ -1,13 +1,13 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {},
-  config = function()
-    require("which-key").setup({})
-    require("chad.mappings").setup()
-  end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {},
+	config = function()
+		require("which-key").setup({})
+		require("chad.mappings").setup()
+	end,
 }
