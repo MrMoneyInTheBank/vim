dffd3395f03cd5ea818aa2ecb7c927f9f09968cc
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2026-03-28 05:14:36 +0530

    idk something i made this change a lot time ago just didnt commit get off my back

--- lua/chad/plugins/lsp-config.lua
+++ lua/chad/plugins/lsp-config.lua
@@ -1,147 +1,154 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		name = "mason",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		name = "mason-tool-installer",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			local installer = require("mason-tool-installer")
+  {
+    "williamboman/mason.nvim",
+    name = "mason",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    name = "mason-tool-installer",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      local installer = require("mason-tool-installer")
 
-			installer.setup({
-				ensure_installed = {
-					"mypy",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		name = "mason-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"pyright",
-					"ruff",
-					"gopls",
-					"rust_analyzer",
-					"clangd",
-					"ts_ls",
-					"zls",
-					"tinymist",
-					"astro",
-					"html",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		name = "nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      installer.setup({
+        ensure_installed = {
+          "mypy",
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    name = "mason-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        automatic_enable = false,
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "ruff",
+          "gopls",
+          "rust_analyzer",
+          "clangd",
+          "ts_ls",
+          "zls",
+          "tinymist",
+          "astro",
+          "html",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    name = "nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = { "html" },
-			})
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html" },
+      })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				filetypes = { "lua" },
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "lua" },
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-				filetypes = { "python" },
-				offset_encoding = "utf-8",
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+        offset_encoding = "utf-8",
+      })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				filetypes = { "go" },
-			})
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+        offset_encoding = "utf-8",
+      })
 
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-				filetypes = { "rust" },
-				settings = {
-					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
-						},
-					},
-				},
-			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        filetypes = { "go" },
+      })
 
-			lspconfig.clangd.setup({
-				init_options = {
-					fallbackFlags = { "--std=c++20" },
-				},
-				capabilities = capabilities,
-				filetypes = { "c", "cpp" },
-			})
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        filetypes = { "rust" },
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
+      })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-				offset_encoding = "utf-8",
-			})
+      lspconfig.clangd.setup({
+        init_options = {
+          fallbackFlags = { "--std=c++20" },
+        },
+        capabilities = capabilities,
+        filetypes = { "c", "cpp" },
+      })
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-			})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        offset_encoding = "utf-8",
+      })
 
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-				filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+      })
 
-			lspconfig.zls.setup({
-				capabilities = capabilities,
-				filetypes = { "zig" },
-			})
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+      })
 
-			lspconfig.tinymist.setup({
-				capabilities = capabilities,
-				filetypes = { "typst" },
-				settings = {
-					formatterMode = "typstyle",
-					exportPdf = "never",
-					semanticTOkens = "disable",
-				},
-			})
+      lspconfig.zls.setup({
+        capabilities = capabilities,
+        filetypes = { "zig" },
+      })
 
-			lspconfig.astro.setup({
-				capabilities = capabilities,
-				filetypes = { "astro" },
-			})
+      lspconfig.tinymist.setup({
+        capabilities = capabilities,
+        filetypes = { "typst" },
+        settings = {
+          formatterMode = "typstyle",
+          exportPdf = "never",
+          semanticTOkens = "disable",
+        },
+      })
 
-			-- mappings
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-		end,
-	},
-	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end,
-	},
+      lspconfig.astro.setup({
+        capabilities = capabilities,
+        filetypes = { "astro" },
+      })
+
+      -- mappings
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
 }
